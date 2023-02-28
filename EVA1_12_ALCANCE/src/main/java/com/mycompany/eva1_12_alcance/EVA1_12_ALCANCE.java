8de8e41a8a8d86daa1929df273a678c4fbd3c062
AlonsoGC03 <124391446+AlonsoGC03@users.noreply.github.com>
2023-02-28 12:42:44 -0600

    Add files via upload

---
+++ EVA1_12_ALCANCE/src/main/java/com/mycompany/eva1_12_alcance/EVA1_12_ALCANCE.java
@@ -0,0 +1,11 @@
+package com.mycompany.eva1_12_alcance;
+
+public class EVA1_12_ALCANCE {
+
+    public static void main(String[] args) {
+        System.out.println(x);//no exixte por que esta declarada despues de la instruccion
+        System.out.println(args);//args esta declarada antes de la instruccion
+        int x = 100;
+        System.out.println(x);
+    }
+}
